d881b6c627b3c0399df380d18c18c73f2bc3df96
uhwickramage <uhwickramage@gmail.com>
2024-06-20 12:27:38 +0530

    changes to app.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { Provider } from 'react-redux';
-    import storeConfig from './store/storeConfig';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-const store = storeConfig();
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Press Me/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
-    test('renders without crashing', () => {
-      const div = document.createElement('div');
-      ReactDOM.render(<Provider store={store}><App /></Provider>, div);
-    });
